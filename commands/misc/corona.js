2ef3ef58c519e58867661251fe9ec701121338b3
Michael Pavlovic <michaelpavlovic87@gmail.com>
2020-04-24 20:21:50 -0400

    Fix issue with old node version

---
+++ commands/misc/corona.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require('discord.js');
+const { green_dark } = require("../../colours.json");
+const { NovelCovid } = require("novelcovid");
+const track = new NovelCovid();
+
+module.exports = {
+    run: async(client, message, args, owner) => {
+        if(!args.length) {
+            return message.channel.send("Enter a country to search for!")
+        }
+          
+        if(args[0] === "all") {
+            let corona = await track.all();
+            
+            let embed = new MessageEmbed()
+            .setTitle("Global Cases")
+            .setColor(green_dark)
+            .addField("Total Cases", corona.cases, true)
+            .addField("Total Deaths", corona.deaths, true)
+            .addField("Total Recovered", corona.recovered, true)
+            .addField("Today's Cases", corona.todayCases, true)
+            .addField("Today's Deaths", corona.todayDeaths, true)
+            .addField("Active Cases", corona.active, true)
+            .setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
+            
+            return message.channel.send(embed);    
+        } else {
+            let corona = await track.countries(args[0]);
+            
+            let embed = new MessageEmbed()
+            .setTitle(`${corona.country}`)
+            .setColor(green_dark)
+            .addField("Total Cases", corona.cases, true)
+            .addField("Total Deaths", corona.deaths, true)
+            .addField("Total Recovered", corona.recovered, true)
+            .addField("Today's Cases", corona.todayCases, true)
+            .addField("Today's Deaths", corona.todayDeaths, true)
+            .addField("Active Cases", corona.active, true)
+            .setFooter(`© ${message.guild.me.displayName}`, client.user.displayAvatarURL());
+            
+            return message.channel.send(embed) ;
+          }
+    },
+    config: {
+        name: 'corona',
+        description: 'Displays data about the corona virus',
+        usage: '!corona all | [country]',
+        category: 'misc',
+        accessableby: 'Members',
+        aliases: ['covid']
+    }
+}
--- events/client/ready.js
+++ events/client/ready.js
@@ -3,9 +3,9 @@ const PREFIX = process.env.PREFIX;
 module.exports = (client) => {
     console.log(client.user.tag + ' is online!');
 
-    client.user.setActivity('my development', { type: 'WATCHING'}).catch(console.error);
+    //client.user.setActivity('my development', { type: 'WATCHING'}).catch(console.error);
 
     //cycle through the activities on a set interval
-    //let activities = [ `${client.guilds.size} servers!`, `${client.channels.size} channels!`, `${client.users.size} users!` ], i = 0;
-    //setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
+    let activities = [ `${client.guilds.size} servers!`, `${client.channels.size} channels!`, `${client.users.size} users!` ], i = 0;
+    setInterval(() => client.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
 }
